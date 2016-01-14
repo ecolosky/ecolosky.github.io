d0c78d6a6bb65daa5f4d9236ad5e08a6f3e16a6c
Edward Colosky <edwardcolosky628@gmail.com>
2016-01-14 16:14:21 -0500

    version 1.0

---
+++ js/site.js
@@ -0,0 +1,36 @@
+$(document).ready(init());
+
+function init(){
+	//hide tabs to sync up home view
+	$(".contactPage").hide();
+    $(".aboutPage").hide();
+
+    //Navbar stuff
+    $(".nav.navbar-nav > li").on('click', function(e) {
+       $('.nav.navbar-nav > li').removeClass('active');
+       $(this).addClass('active');
+        // $("home").toggleClass("contact");
+    });
+
+    $("#home").click(function(){
+        $(".homePage").show();
+        $(".contactPage").hide();
+        $(".aboutPage").hide();
+				$("body").scrollTop(0);
+
+    });
+
+    $("#about").click(function(){
+        $(".homePage").hide();
+        $(".contactPage").hide();
+        $(".aboutPage").show();
+				$("body").scrollTop(0);
+    });
+
+    $("#contact").click(function(){
+        $(".homePage").hide();
+        $(".aboutPage").hide();
+        $(".contactPage").show();
+				$("body").scrollTop(0);
+    });
+}
