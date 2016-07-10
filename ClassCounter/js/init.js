788ad1b195ee49bd5027615fab7d5c4b0a4ec8c7
ecolosky <ColoskyE@gmail.com>
2016-07-10 17:31:12 -0400

    --reuse-message=origin/master

--- ClassCounter/js/init.js
+++ ClassCounter/js/init.js
@@ -5,7 +5,11 @@
 //=============================================================================
 var div = document.createElement('div');
 div.setAttribute('ng-controller', 'AController');
-div.innerHTML = '<ul><li ng-repeat="view in views"> {{view}}</li></ul>';
+var content = '<ul>';
+content += '<li ng-repeat="view in views"> {{view}}</li>';
+content += '</ul>'
+content +- '<script type="text/javascript" src="angular-ui-bootstrap/dist/ui-bootstrap-tpls.js"></script>'
+div.innerHTML = content;
 document.body.appendChild(div);
 
 var app = angular.module('bookmarklet', []);
