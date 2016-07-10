7f9f27d95f234e4d8bbe700dfcb08729e7815445
ecolosky <ColoskyE@gmail.com>
2016-07-10 18:04:15 -0400

    --reuse-message=origin/master

--- ClassCounter/js/init.js
+++ ClassCounter/js/init.js
@@ -7,12 +7,11 @@ var div = document.createElement('div');
 div.setAttribute('ng-controller', 'AController');
 var content = '<ul>';
 content += '<li ng-repeat="view in views"> {{view}}</li>';
-content += '</ul>'
-content += '<script type="text/javascript" src="../angular-ui-bootstrap/dist/ui-bootstrap-tpls.js"></script>'
+content += '</ul>';
 div.innerHTML = content;
 document.body.appendChild(div);
 
-var app = angular.module('bookmarklet', ['ui.bootstrap']);
+var app = angular.module('bookmarklet', []);
 
 app.run(function ($rootScope) {
   $rootScope.URL = location.href;
