046cf1eb153accf88bdebfe570c01e80d9567aad
ecolosky <ColoskyE@gmail.com>
2016-07-11 16:46:23 -0400

    lists and tallies css classes

--- ClassCounter/js/controller.js
+++ ClassCounter/js/controller.js
@@ -7,14 +7,13 @@ app.controller('AController', function($scope, $http) {
   $scope.views = ['a', 'b', 'c'];
   // populate class objects
   $scope.classArr = [];
-  var classCnt = {};
     [].forEach.call(document.querySelectorAll("*"), function(element) {
       if (element.className) {
         var tempClassArr = element.className.split(" ");
         for (var i = 0; i < tempClassArr.length; i++) {
           for(var x = 0; x < $scope.classArr.length; x++){
             if (tempClassArr[i] == $scope.classArr[x].class) {
-              classArr.push({class: tempClassArr[i], count: 0});
+              $scope.classArr.push({class: tempClassArr[i], count: 0});
             }
             $scope.classArr[x].count = $scope.classArr[x].count + 1;
           }
