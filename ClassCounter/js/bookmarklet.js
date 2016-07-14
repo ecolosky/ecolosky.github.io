ed36cd30d2248cd06b499a080375cb4e87dac7b0
ecolosky <ColoskyE@gmail.com>
2016-07-14 17:23:08 -0400

    1.4 - back to http

--- ClassCounter/js/bookmarklet.js
+++ ClassCounter/js/bookmarklet.js
@@ -5,7 +5,7 @@
 // =================================================================
 (function(){
   // remote code location
-  var appRoot = 'https://edcolosky.com/ClassCounter/';
+  var appRoot = 'http://edcolosky.com/ClassCounter/';
 
   // Load the script from url and when it's ready loading run the callback.
   function loadScript(url, callback) {
